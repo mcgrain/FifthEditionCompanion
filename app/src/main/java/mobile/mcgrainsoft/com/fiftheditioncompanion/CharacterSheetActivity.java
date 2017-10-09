1136f87c9cbc7b0db0c4e266e5c43b31a079cc21
Brandon McGrain <mcgraindead@gmail.com>
2017-10-08 23:25:26 -0400

    SnackBar text change

--- app/src/main/java/mobile/mcgrainsoft/com/fiftheditioncompanion/CharacterSheetActivity.java
+++ app/src/main/java/mobile/mcgrainsoft/com/fiftheditioncompanion/CharacterSheetActivity.java
@@ -55,7 +55,7 @@ public class CharacterSheetActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Invite Party Members?", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
